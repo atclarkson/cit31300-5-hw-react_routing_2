55f08800c0d682c1f8a7e580c969b8a31aae4516
Adam Clarkson <atclarkson@gmail.com>
2020-02-14 21:53:42 -0500

    Add hot movies list

--- src/components/Cool.js
+++ src/components/Cool.js
@@ -6,15 +6,25 @@ function Cool() {
 
     useEffect(() => {
         fetchMovies();
+        fetchHot();
     }, []);
 
     const [movies, setMovies] = useState([]);
+    const [hotMov, setHotMov] = useState([]);
 
     const fetchMovies = async () => {
         const data = await fetch('https://www.omdbapi.com/?apikey=ea518254&s=cool');
         const movies = await data.json();
         console.log(movies.Search);
         setMovies(movies.Search);
+    }
+
+
+    const fetchHot = async () => {
+        const data = await fetch('https://www.omdbapi.com/?apikey=ea518254&s=hot');
+        const movies = await data.json();
+        console.log(movies.Search);
+        setHotMov(movies.Search);
     }
 
     return (
@@ -31,6 +41,20 @@ function Cool() {
                     </Link>
                 ))}
             </div>
+
+            <h1>We can do <span className="hot-movies">HOT</span> Movies Too:</h1>
+            <div className="movies">
+
+                {hotMov.map(movie => (
+                    <Link key={movie.imdbID} to={`/cool/${movie.imdbID}`}>
+                        <div className="movie">
+                            <img src={movie.Poster} alt={movie.Title}/>
+                            <h2>{movie.Title}</h2>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+
         </div>
     );
 }
